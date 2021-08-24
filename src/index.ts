c1d212625efa4dea0a78128d11496a58b0fc0df4
David <xudavidb@gmail.com>
2021-08-23 18:18:07 -0700

    initial server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-"use strict";
-function sum(num1: number, num2: number) {
-  return num1 + num2;
-}
-console.log(sum(8, 4));
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res): void => {
+  res.send("Hello Typescript with Node.js!");
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Server Running here 👉 https://localhost:${PORT}`);
+});
